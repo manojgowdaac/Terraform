b6693f66694de2a2146d09d23179d64a927e6014
MANOJ GOWDA AC <95601278+manojgowdaac@users.noreply.github.com>
2024-11-11 12:13:42 +0530

    Delete ec2_instance/instances directory

--- ec2_instance/instances/output.tf
+++
@@ -1,6 +0,0 @@
-#output
-
-output "instance_id"{
-    description = "ID fo the ec2 instance"
-    value = aws_instance.project.id
-}
--- ec2_instance/instances/provider.tf
+++
@@ -1,5 +0,0 @@
-#provider
-#you can create new file provider.tf can you can store there also
-provider "aws"{
-    region = "ap-southeast-2"
-}
